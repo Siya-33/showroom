aeb2cffd2a5d9fc3984857dec4fd6fdf120cde50
MIN202299 <min202299@gmail.com>
2023-08-01 18:33:54 +0800

    chore:store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stage } from '@react-three/drei'
+import CurvedSurface from './components/CurvedSurface'
+
 function App() {
   return (
-    <div className="screen-bg h-100vh w-100vw">
-      <span>works</span>
-      <p>eer</p>
+    <div className="h-100vh w-100vw">
+      <div className="absolute inset-0">
+        <Canvas>
+          <ambientLight />
+          <pointLight />
+          <Stage>
+            <CurvedSurface />
+          </Stage>
+          <OrbitControls makeDefault minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} maxZoom={0} />
+        </Canvas>
+      </div>
+      <div className="text-6xl text-gray">
+        works!
+      </div>
     </div>
   )
 }
